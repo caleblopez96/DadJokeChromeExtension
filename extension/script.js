c9830706349adee29d15d01c637e55a463abc211
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-04-06 17:22:53 -0700

    Add files via upload

---
+++ extension/script.js
@@ -0,0 +1,19 @@
+const jokeText = document.querySelector(".jokeText");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", getJoke);
+
+function getJoke() {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json"
+    }
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const joke = data.joke;
+      jokeText.innerText = `${joke}😂`;
+    });
+}
